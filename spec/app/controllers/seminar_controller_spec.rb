52396268a5be6e62e6aef802f23d7aea86e7a94b
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-10-06 09:10:46 +0200

    controller and helper specs

--- spec/app/controllers/seminar_controller_spec.rb
+++ spec/app/controllers/seminar_controller_spec.rb
@@ -5,8 +5,28 @@ RSpec.describe "SeminarController" do
     before do
       get "/seminar/"
     end
-  
+
     it "contains the test seminar" do
+      expect(last_response.body).to include("Testing Seminar")
+    end
+  end
+
+  describe "halts for invalid id/shorturl" do
+    before do
+      get "/seminar/show/no_seminar"
+    end
+
+    it "containing the seminar name" do
+      expect(last_response.status).to eq(404)
+    end
+  end
+
+  describe "routes to seminar using short_url" do
+    before do
+      get "/seminar/show/testing_seminar"
+    end
+
+    it "containing the seminar name" do
       expect(last_response.body).to include("Testing Seminar")
     end
   end
@@ -15,7 +35,7 @@ RSpec.describe "SeminarController" do
     before do
       get "/seminar/show/1"
     end
-  
+
     it "containing the seminar name" do
       expect(last_response.body).to include("Testing Seminar")
     end
@@ -24,6 +44,45 @@ RSpec.describe "SeminarController" do
     end
     it "containing the seminar date (german format)" do
       expect(last_response.body).to include("31.12.2014")
+    end
+    it "containing the seminar description" do
+      expect(last_response.body).to include("everything you")
+    end
+    it "containing notices" do
+      expect(last_response.body).to include("Preis")
+    end
+    it "containing Registration link" do
+      expect(last_response.body).to include("Anmeldung")
+    end
+    it "containing the referee names" do
+      expect(last_response.body).to include("John Doe")
+    end
+    it "shows related seminars" do
+      expect(last_response.body).to include("Advanced")
+    end
+  end
+
+  describe "shows category overview" do
+    before do
+      get "/seminar/by_category/soulcleaning"
+    end
+
+    it "containing the test seminar" do
+      expect(last_response.body).to include("Testing Seminar")
+    end
+  end
+
+  describe "shows Referee pages" do
+    before do
+      get '/seminar/referee/1'
+    end
+
+    it 'shows the full name' do
+      expect(last_response.body).to include('John Doe')
+    end
+    it 'shows held seminars' do
+      expect(last_response.body).to include("Testing Seminar")
+      expect(last_response.body).to include("Testing Seminar Advanced")
     end
   end
 end
--- spec/app/helpers/seminar_helper_spec.rb
+++ spec/app/helpers/seminar_helper_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe "Gusy::App::SeminarHelper" do
   before { helpers.extend Gusy::App::SeminarHelper }
   subject { helpers }
 
-  it "should return nil" do
+  it "should return nil for invalid seminar" do
     expect(subject.seminar('boo')).to be_nil
+  end
+
+  pending do
+    it "should make a seminar link available, preferably shorturl" do
+      expect(subject.seminar_link subject.seminar(1)).to include "short"
+    end
   end
 end
