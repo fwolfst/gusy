010004c48bca07483865fd740431f50ed512a3e4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-10-11 22:29:09 +0200

    add more seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ seminar_1  = Seminar.create(:name => "Testing Seminar",
                             :shorturl => 'testing_seminar',
                             :uuid => "uuid-like-string",
                             :description => "learn everything you want",
-                            :web_notice => '[{"title": "Preis", "value":"10 Euro"}]',
+                            :web_notice => '[{"label": "Preis", "text":"10 Euro"}]',
                             :cancellation_conditions => 'no problem, three days before')
 
 category_1 = Category.create(:name => "Soulcleaning",
@@ -32,6 +32,15 @@ seminar_3  = Seminar.create(:name => "Weihnachtsseminar",
                             :description => "includes cookies"
                             )
 seminar_3.save
+
+seminar_4  = Seminar.create(:name => "Vergangenheit erleben",
+                            :date_from => DateTime.new(2012, 03, 2, 9, 9),
+                            :date_to => DateTime.new(2012, 03, 5, 10, 10),
+                            :shorturl => 'pasting',
+                            :uuid => "uuid-like-string4",
+                            :description => "will be somewhat weird"
+                            )
+seminar_4.save
 
 referee = Referee.create(:first_name => 'John', :last_name => 'Doe')
 referee.add_seminar(seminar_1)
