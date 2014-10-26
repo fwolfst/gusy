f600577abc18829177ebf84d5585a3048a7d4223
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-10-26 15:09:42 +0100

    update test seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@ seminar_1  = Seminar.create(:name => "Testing Seminar",
                             :shorturl => 'testing_seminar',
                             :uuid => "uuid-like-string",
                             :description => "learn everything you want",
-                            :web_notice => '[{"label": "Preis", "text":"10 Euro"}]',
+                            :web_notices => '[{"label": "Preis", "text":"10 Euro"}]',
+                            :web_notice => '<strong>weak participants wanted</strong>',
                             :cancellation_conditions => 'no problem, three days before')
 
 category_1 = Category.create(:name => "Soulcleaning",
@@ -40,7 +41,18 @@ seminar_4  = Seminar.create(:name => "Vergangenheit erleben",
                             :uuid => "uuid-like-string4",
                             :description => "will be somewhat weird"
                             )
+seminar_4.add_category category_1
 seminar_4.save
+
+seminar_future = Seminar.create(:name => "Sci-Fi Experience",
+                            :date_from => DateTime.new(12012, 03, 2, 9, 9),
+                            :date_to => DateTime.new(12012, 03, 5, 10, 10),
+                            :shorturl => 'scifi',
+                            :uuid => "uuid-like-string5",
+                            :description => "will be super weird"
+                            )
+seminar_future.add_category category_1
+seminar_future.save
 
 referee = Referee.create(:first_name => 'John', :last_name => 'Doe')
 referee.add_seminar(seminar_1)
