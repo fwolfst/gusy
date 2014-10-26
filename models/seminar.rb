983dd703de8252ab47f2786f0205c49374fd32c5
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-10-26 15:21:36 +0100

    add helper methods to seminar model (fetch time scoped).

--- models/seminar.rb
+++ models/seminar.rb
@@ -1,9 +1,45 @@
 require 'json'
+require 'app'
+require 'helpers/date_helper'
 
 class Seminar < Sequel::Model
+  class DateHelperClass
+    include Gusy::App::DateHelper
+  end
+  @@date_helper = DateHelperClass.new
+
   many_to_many :categories, :join_table => :seminars_categories
   many_to_many :referees, :join_table => :seminars_referees
-  def web_notices
-    JSON.parse web_notice rescue []
+
+  # json parse the web notice array
+  def get_web_notices
+    JSON.parse web_notices rescue []
+  end
+
+  # Scoped by time.
+  dataset_module do
+    def in_this_year
+      year_begin, year_end = @@date_helper.this_year_span()
+      where{date_from >= year_begin && date_from <= year_end}
+    end
+    def in_future
+      where{date_from >= Date.today}
+    end
+    def in_nearer_future
+      year_begin, year_end = @@date_helper.this_year_span()
+      where{date_from >= year_begin && date_from <= year_end}
+    end
+    def three_month
+      span_begin, span_end = @@date_helper.three_month
+      where{date_from >= span_begin}.and{date_from <= span_end}
+    end
+    def of_year year
+      where{date_from >= Date.civil(year, 1, 1)}.and{
+           date_from <= Date.civil(year + 1, 12, 31)}
+    end
+    def of_month year, month
+      where{date_from >= Date.civil(year, month, 1)}.and{
+           date_from <= Date.civil(year, month, -1)}
+    end
   end
 end
