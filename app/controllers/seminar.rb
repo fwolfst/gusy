fffe0003ba70a467ae5374ebcc6806fe992398c4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2015-01-08 12:24:15 +0100

    show seminars on GET /seminar/[:uuid]

--- app/controllers/seminar.rb
+++ app/controllers/seminar.rb
@@ -98,6 +98,13 @@ Gusy::App.controllers :seminar do
     render 'registration'
   end
 
+  get /(.*)/ do
+    seminar_id_uuid = params[:captures].first.split("/").last
+    @seminar = seminar(seminar_id_uuid) || halt(404, "Seminar not found")
+    @title = "Sieben Linden: Seminar #{@seminar.name}"
+    render 'show'
+  end
+
   post :register do
     logger.info params
     @seminar = seminar(params['seminar_id']) || halt(404, t("seminars.not_found"))
